e9e5ec000f4c88f1908a644507047538445a2dab
myeongjun1007 <137693966+myeongjun1007@users.noreply.github.com>
2023-07-02 03:30:09 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,22 @@
+import sys
+N = int(sys.stdin.readline())
+
+for i in range(0,2*N-1):
+    for j in range(0,2*N-1):
+        if i<(2*N-1)//2:
+            if j<(2*N-1)//2-i :
+                print(" ",end="")
+            elif ((2*N-1)//2-i<=j) and ((2*N-1)//2+i>=j):
+                print("*",end="")
+
+        elif i>=(2*N-1)//2:
+            if j<i-(2*N-1)//2:
+                print(" ",end="")
+            elif (i-(2*N-1)//2<=j) and (2*N-2-i+(2*N-1)//2>=j):
+                print("*",end="")
+
+
+    if i==2*N-2:
+        continue
+    else:
+        print()
