3671e2d5b4cb13fb4b9abcfc0bffe2955e9b96b7
myeongjun1007 <137693966+myeongjun1007@users.noreply.github.com>
2023-07-26 04:04:25 +0900

    [Silver II] Title: 유기농 배추, Time: 80 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,47 @@
+import sys
 from collections import deque
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+T = int(sys.stdin.readline())
 
-t = int(input())
+direction_x = [0,0,1,-1]
+direction_y = [1,-1,0,0]
+def bfs(field,row,col):
+    que = deque()
+    que.append((row,col))
+    field[row][col] = 0
 
-def bfs(graph, a, b):
-    queue = deque()
-    queue.append((a,b))
-    graph[a][b] = 0
+    while que:
+        curr_x, curr_y = que.popleft()
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+        for l in range(4):
+            next_x = curr_x + direction_x[l]
+            next_y = curr_y + direction_y[l]
+
+            if next_x < 0 or next_x >= n or next_y < 0 or next_y >= m:
                 continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
+
+            if field[next_x][next_y] == 1:
+                field[next_x][next_y] = 0
+                que.append((next_x,next_y))
+
     return
 
-for i in range(t):
+
+
+for i in range(T):
     cnt = 0
-    n, m, k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
+    n, m, k = map(int,sys.stdin.readline().split())
+    field = [[0]*m for _ in range(n)]
 
     for j in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
+        x,y = map(int,sys.stdin.readline().split())
+        field[x][y] =1
 
     for a in range(n):
         for b in range(m):
-            if graph[a][b] == 1:
-                bfs(graph, a, b)
+            if field[a][b] == 1:
+                bfs(field, a, b)
+
                 cnt += 1
+
     print(cnt)
